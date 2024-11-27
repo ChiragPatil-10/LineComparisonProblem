3c4669ce44d7e1a7f294924f037c7f7e098bbda0
Chirag Patil <patilchirag192k@gmail.com>
2024-11-27 12:26:06 +0530

    Added : UC2 Line Equality Check

--- src/LineComparisonProblem.java
+++ src/LineComparisonProblem.java
@@ -19,6 +19,26 @@ public class LineComparisonProblem {
         double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("The length of the line is: " + length);
 
+        System.out.println("Enter coordinates of the first point of line 2 x3: ");
+        double x3 = sc.nextDouble();
+        System.out.println("Enter coordinates of the first point of line 2 y3: ");
+        double y3 = sc.nextDouble();
+
+        System.out.println("Enter coordinates of the second point of line 2 x4: ");
+        double x4 = sc.nextDouble();
+        System.out.println("Enter coordinates of the second point of line 2 y4: ");
+        double y4 = sc.nextDouble();
+
+        double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+        System.out.println("The length of the line is: " + length2);
+
+        if(Double.valueOf(length).equals(Double.valueOf(length2))){
+            System.out.println("Two lines are equal in length");
+        } else{
+            System.out.println("Two lines are not equal in length");
+        }
+
         sc.close();
+
     }
 }
